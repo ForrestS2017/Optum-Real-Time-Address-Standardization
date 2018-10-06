179141c15151d8cfb15b4147caa2a1c4dd275788
Forrest Smith <fsmith2017@gmail.com>
2018-10-06 15:41:12 -0400

    Main test

---
+++ standardize.py
@@ -0,0 +1,9 @@
+# Standardize input data by autocorrecting typos, fulfilling area codes, and formatting punctuation
+# Authors: Wesley Cheung, Parker Fisher, Dennis Yu, Forrest Smith
+
+import autocorrect as ac
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__": main()
